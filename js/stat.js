b5fb99e585e1c97682125888b120fbc9a02ebff3
Ekaterina Elesova <elesova.e@gmail.com>
2017-11-28 03:02:10 +0300

    правки по заданию 2

--- js/stat.js
+++ js/stat.js
@@ -1,54 +1,58 @@
-var renderStatistics = function (ctx, names, times) {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-    ctx.fillRect(110, 20, 420, 270);
+'use strict';
 
-    ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
-    ctx.strokeRect(100, 10, 420, 270);
-    ctx.fillRect(100, 10, 420, 270);
+var getRandomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
-    ctx.fillStyle = '#000';
-    ctx.font = '16px PT Mono';
-
-    ctx.fillText('Ура вы победили!', 120, 40);
-
-    var max = -1;
-
-    for(var i = 0 ; i < times.length; i++ ) {
-      var time = times[i];
-      if (time > max) {
-        max = time;
-      }
+var getMaxValue = function (array, max) {
+  for(var i = 0 ; i < array.length; i++ ) {
+    if (array[i] > max) {
+      max = array[i];
     }
+  }
 
-    ctx.fillText('Список результатов:', 120, 60);
+  return max;
+}
 
-    var histoHeight = 150;
-    var histoX = 150;
-    var step = histoHeight / max;
-    var columnIndent = 90;
+var drawHistogram = function (ctx, name, time, step, height, histoX, histoHeight, columnIndent, index) {
+  ctx.fillStyle = '#000';
+  ctx.fillText(time.toFixed(0), histoX + columnIndent * index, 90 + (histoHeight - height));
 
-    for (var i = 0; i < times.length; i++) {
-      var name = names[i];
-      var time = times[i];
+  name === 'Вы' ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : ctx.fillStyle = 'rgba(0,0,255,0.' + getRandomNumber(1, 9) + ')';
 
-      var height = step * time;
+  ctx.fillRect(histoX + columnIndent * index, 100 + (histoHeight - height), 40, height);
 
-      ctx.fillStyle = '#000';
-      ctx.fillText(time.toFixed(0), histoX + columnIndent * i, 90 + (histoHeight - height));
+  ctx.fillStyle = '#000';
+  ctx.fillText(name, histoX + columnIndent * index, 100 + histoHeight + 20);
+}
 
-      var getRandomNumber = function (min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-      };
+window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
 
-      if (names[i] === 'Вы') {
-        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      } else {
-        ctx.fillStyle = 'rgba(0,0,255,0.' + getRandomNumber(1, 9) + ')';
-      }
+  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
+  ctx.strokeRect(100, 10, 420, 270);
+  ctx.fillRect(100, 10, 420, 270);
 
-      ctx.fillRect(histoX + columnIndent * i, 100 + (histoHeight - height), 40, height);
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
 
-      ctx.fillStyle = '#000';
-      ctx.fillText(name, histoX + columnIndent * i, 100 + histoHeight + 20);
-    }
-  };
+  ctx.fillText('Ура вы победили!', 120, 40);
+
+  var max = -1;
+
+  max = getMaxValue(times, max);
+
+  ctx.fillText('Список результатов:', 120, 60);
+
+  var histoHeight = 150;
+  var histoX = 150;
+  var step = histoHeight / max;
+  var columnIndent = 90;
+  var height;
+
+  for (var i = 0; i < times.length; i++) {
+    height = step * times[i];
+    drawHistogram (ctx, names[i], times[i], step, height, histoX, histoHeight, columnIndent, i)
+  }
+};
